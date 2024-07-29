52d3437fc899b920709c1f35de26d8cbef0e9544
driri <ksgkdw78@naver.com>
2024-07-29 21:57:49 +0900

    menu add

--- src/components/product/doll/Menu.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Menu = () => {
-  return <nav className="product_menu_box">메뉴</nav>;
-};
-
-export default Menu;
---
+++ src/components/product/index.js
@@ -0,0 +1,8 @@
+import DollBox from "./doll/DollBox";
+import Search from "./doll/Search";
+import BasketShort from "./doll/BasketShort";
+import Paging from "./doll/Paging";
+
+import Menu from "./TypeMenu/Menu";
+
+export { DollBox, Search, BasketShort, Paging, Menu };
