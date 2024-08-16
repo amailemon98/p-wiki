2ce464b2c9b6c7fcc21738ab774e3a81352a51de
amailemon98 <amailemon98@gmail.com>
2024-08-16 21:47:25 +0900

    loading screen height fixed

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -4,8 +4,8 @@ import { PacmanLoader } from 'react-spinners'
 const Loading = () => {
 
   return (
-    <div className='flex justify-center items-center w-full h-[85vh]'>
-        <div className='w-[300px] h-[300px] flex flex-col justify-center items-center'>
+    <div className='flex justify-center items-center w-full h-screen'>
+        <div className='w-[300px] h-[300px] flex flex-col justify-center items-center -mt-[25vh]'>
           <PacmanLoader
             color={"#f73e44"}
             size={50}
